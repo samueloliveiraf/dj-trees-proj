33797afad7fbd57650847e230f00d2f234224f00
samueloliveiraf <samueldiablo73@gmail.com>
2024-06-18 23:23:54 -0300

    first commit

---
+++ app_web/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from apps.accounts import urls as urls_account, views as views_account
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('account/', include(urls_account)),
+    path('',  views_account.dashboard, name='dashboard')
+]
